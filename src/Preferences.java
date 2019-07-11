70eb81adb03db03964f99b010305ba446be63a49
bridges-wood <bridges.wood@gmail.com>
2019-07-11 12:07:20 +0100

    Updates to backend java.

---
+++ src/Preferences.java
@@ -0,0 +1,10 @@
+
+public class Preferences {
+	public double leagueTablePref;
+	public double studentSatisfactionPref;
+	public double employabilityPref;
+	public double researchQualityPref;
+	public double studentToStaffPref;
+	public double costOfLivingPref;
+	public double internationalStudentPref;
+}
---
+++ src/Server.java
@@ -0,0 +1,103 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+
+	public static void main(String[] args) {
+		new Server().startServer();
+	}
+
+	public void startServer() {
+		final ExecutorService clientProcessingPool = Executors.newCachedThreadPool(); // Creates our pool of threads to
+																						// process incoming text from
+																						// clients.
+		Runnable serverTask = new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					@SuppressWarnings("resource") // Because the server should always be up, the socket is never closed
+													// programmatically.
+					ServerSocket serverSocket = new ServerSocket(8000);
+					System.out.println("Waiting for clients to connect...");
+					while (true) {
+						Socket clientSocket = serverSocket.accept();
+						// If any task comes through, a thread is automatically assigned to it.
+						clientProcessingPool.submit(new ClientTask(clientSocket));
+					} // need host and port, we want to connect to the ServerSocket at port 8000
+				} catch (IOException e) {
+					System.err.println("Unable to process client request");
+					e.printStackTrace();
+				}
+			}
+		};
+		Thread serverThread = new Thread(serverTask);
+		serverThread.start();
+	}
+
+	private class ClientTask implements Runnable {
+		private final Socket clientSocket;
+
+		private ClientTask(Socket clientSocket) {
+			this.clientSocket = clientSocket;
+		}
+
+		@Override
+		public void run() {
+			System.out.println("Got a client !");
+			String toSend = receive(clientSocket);
+			// Take JSON and send to generate JSON.
+			System.out.println("Data from Client: " + toSend);
+			try {
+				send(toSend, clientSocket);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		/**
+		 * Sends a string over a specified socket, back to the client that submitted the
+		 * request.
+		 * 
+		 * @param dataString
+		 *            The string to be sent.
+		 * @param socket
+		 *            The socket of the client.
+		 * @throws IOException
+		 */
+		public void send(String dataString, Socket socket) throws IOException {
+			OutputStream outputStream = socket.getOutputStream();
+			DataOutputStream out = new DataOutputStream(outputStream); // Creates an output stream to the other end of
+																		// the socket.
+			System.out.println("Sending messages to the client");
+			out.writeUTF(dataString); // Writes data to the client.
+			System.out.println("Closing socket and terminating thread.");
+			out.flush(); // Cleans buffer.
+		}
+
+		/**
+		 * Receives string data from the given client socket.
+		 * 
+		 * @param socket
+		 *            The clients socket.
+		 * @return The string sent by the client.
+		 */
+		public String receive(Socket socket) {
+			try {
+				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				String content = br.readLine();
+				// Parse content as Json and return.
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+	}
+
+}
